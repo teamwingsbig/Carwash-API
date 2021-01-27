5bcc27dfc81e7fad3d2f884fa638083d90bcca15
Sufail Kalathil <sufailkalathil@gmail.com>
2021-01-27 15:45:33 +0530

    create service

--- app/Controler/service.controller.js
+++ app/Controler/service.controller.js
@@ -2,42 +2,34 @@ const Service = require('../Models/service.model');
 
 exports.createService = (req, res) => {
 
-   
-    const sub_service =  req.body.sub_services
 
-   
+    const sub_service = req.body.sub_services
+
+
     const sub_serviceArray = [];
     const varientArray = [];
-   
-    sub_service.forEach(element => {
-         
-        for(i of element.varients){
-            varientArray.push(element.varients)
+
+    for (const service of sub_service) {
+        for (const varients of service.varients) {
+            varientArray.push(varients)
         }
-        console.log(varientArray)
-
         sub_serviceArray.push({
-            name:element.name,
-            brand_id:element.brand_id,
-            varients:varientArray
-            
-            
+            name: service.name,
+            brand_id: service.brand_id,
+            varients: varientArray
         })
-       
-    });
 
-    const postArray = new Service({
+    }
+    const service = new Service({
         title: req.body.title,
         description: req.body.description,
         type: req.body.type,
-        sub_services:sub_serviceArray
-       
-    })
+        sub_services: (sub_serviceArray)
+    });
 
 
-
-
-    console.log(postArray);
+    console.log(service)
+    res.send(service)
     // console.log(title,description,type)
 
 }
