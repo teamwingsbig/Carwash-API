31779e06a70ee3a8b4101f733e4a152180457915
codewidhypno <bibinbabuman@gmail.com>
2021-01-30 13:29:16 +0530

    include new package : express-fileupload

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const url = 'mongodb://localhost/washbay'
 const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+
 
 
 
@@ -15,8 +17,13 @@ con.on('open', () => {
     console.log('connected...')
 })
 
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(express.urlencoded({extended:false}));
+app.use(fileUpload())
+app.use(express.static('./app/Files/Logo/'))
+app.use(express.static('./app/Files/Qrcode/'))
 
 
 
@@ -30,8 +37,10 @@ app.get('/', (req, res) => {
 // routes
 require('./app/Routes/service.routes')(app);
 require('./app/Routes/brand.routes')(app);
+require('./app/Routes/company.routes')(app);
 
 
 app.listen(9000, () => {
     console.log('Server started')
 })
+1
