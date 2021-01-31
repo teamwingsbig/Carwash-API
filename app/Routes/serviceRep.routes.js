22ea38d6bd8e4494a15a0d161c0eabcdec507e30
codewidhypno <bibinbabuman@gmail.com>
2021-01-31 10:22:34 +0530

    Service Rep Module Added

---
+++ app/Routes/serviceRep.routes.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+
+    const serviceRep = require('../Controler/service_rep.controller')
+
+    app.post('/servicerep', serviceRep.createServiceRep);
+    app.get('/servicerep', serviceRep.getServiceRep);
+    app.get('/servicerep/:id', serviceRep.getSingleServiceRep);
+    app.put('/servicerep/:id', serviceRep.removeServiceRep);
+    app.patch('/servicerep/:id', serviceRep.updateServiceRep);
+}
