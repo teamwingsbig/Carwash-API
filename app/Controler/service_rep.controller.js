d7cf41209f336e041a7b2475c1eb14fbe80fa809
codewidhypno <bibinbabuman@gmail.com>
2021-02-06 21:34:51 +0530

    Authentication Module Added

--- app/Controler/service_rep.controller.js
+++ app/Controler/service_rep.controller.js
@@ -1,4 +1,5 @@
 const ServiceRep = require('../Models/service_rep.model') 
+const User       = require('../Models/users.model')
 const Response   = require('../helper/response')
 const emailValidator = require('email-validator')
 
@@ -43,6 +44,17 @@ exports.createServiceRep = (req, res) => {
             return Response.sendFailedmsg(res,'Password Mismatch')
         }
 
+        ServiceRep.findOne({password:password}).then((data) => {
+            if(data) {
+                return Response.sendFailedmsg(res,'Password Already In Use! Please Try With Another') 
+            }
+        })
+        ServiceRep.findOne({passcode:passcode}).then((data) => {
+            if(data) {
+                return Response.sendFailedmsg(res,'Passcode Already In Use! Please Try With Another') 
+            }
+        })
+
        
 
         const servicerep =  new ServiceRep({
@@ -53,8 +65,21 @@ exports.createServiceRep = (req, res) => {
             password:password
         })
 
+        const user =  new User({
+            username:email,
+            password:password,
+            passcode:passcode,
+            isadmin:false
+        })
+
         servicerep.save().then((data) => {
-            return Response.sendSuccessmsg(res,'New Service Rep Added')
+            user.save().then((userdata) => {
+                return Response.sendSuccessmsg(res,'New Service Rep Added')
+            })
+            .catch(err => {
+                return Response.sendFailedmsg(res,'Failed To Add Service Rep',err.message)
+            })
+            
         })
         .catch(err=> {
             return Response.sendFailedmsg(res,'Failed To Add Service Rep',err.message)
@@ -129,7 +154,7 @@ exports.updateServiceRep = (req, res) => {
 
         const AlphaRegEx = /^(?!-)[a-zA-Z-]*[a-zA-Z]$/
 
-        const { name, mobile, email, passcode, confirmpasscode, password, confirmpassword } = req.body
+        const { name, mobile, email} = req.body
 
         if(!name || !mobile || !email || !passcode || !password) {
             return Response.sendFailedmsg(res,'Please Fill All Fields')
@@ -147,37 +172,17 @@ exports.updateServiceRep = (req, res) => {
             return Response.sendFailedmsg(res,'Invalid Email Address')
         }
 
-        if(passcode.length != 4) {
-            return Response.sendFailedmsg(res,'Passcode Length Should Be 4')
-        }
+      
 
-        if(password.length < 6 || password.length > 8) {
-            return Response.sendFailedmsg(res,'Password Length Should Be Minimum  Of 6 or Maximum Length Of 8')
-        }
 
-        if(passcode != confirmpasscode) {
-            return Response.sendFailedmsg(res,'Passcode Mismatch')
-        }
-        if(password != confirmpassword) {
-            return Response.sendFailedmsg(res,'Password Mismatch')
-        }
 
-       
 
-        const servicerep =  new ServiceRep({
-            name:name,
-            mobile:mobile,
-            email:email,
-            passcode:passcode,
-            password:password
-        })
 
         ServiceRep.findOneAndUpdate({_id:req.params.id},{
             name:name,
             mobile:mobile,
-            email:email,
-            passcode:passcode,
-            password:password
+            email:email
+            
         }).then(data=> {
             return Response.sendSuccessmsg(res,'Service Rep Details Updated')
         })
@@ -189,3 +194,5 @@ exports.updateServiceRep = (req, res) => {
         return Response.sendFailedmsg(res,'Failed To Update Service Rep Details',err.message)
     }
 }
+
+
---
+++ app/Controler/users.controller.js
@@ -0,0 +1,89 @@
+const User  = require('../Models/users.model')
+const Response = require('../helper/response')
+
+
+exports.createAdmin = (req, res) => {
+    try {
+
+        const { username, password } = req.body
+
+        const user = new User({
+            username:username,
+            passcode:password,
+            password:password,
+            isadmin:true
+        })
+
+        user.save().then((data) => {
+            return Response.sendFailedmsg(res,'Admin Added')
+        })
+        .catch(err => {
+            return Response.sendFailedmsg(res,'Failed To Add Admin',err.message)
+        })
+
+
+    }
+    catch(err) {
+        return Response.sendFailedmsg(res,'Failed To Add Admin',err.message)
+    }
+}
+
+
+exports.getUsers = (req,res) => {
+    try {
+         User.find().then((data) => {
+             res.send(data)
+         })
+         .catch(err => {
+             res.send(err.message)
+         })
+     }
+     catch(err) {
+         res.send(err.message)
+     }
+     
+ }
+
+
+ exports.authentication = (req, res) => {
+
+     try {
+
+        const { passcode, password} = req.body
+
+
+        if(passcode) {
+            User.findOne({passcode:passcode})
+            .then((authuser) => {
+                if(!authuser) {
+                    return Response.sendFailedmsg(res,'Authentication Failed')
+                }
+                else {
+                    return Response.sendSuccessmsg(res,'Authenticated!',{isAdmin:authuser.isadmin, email:authuser.username})
+                }
+            })
+            .catch(err => {
+                return Response.sendFailedmsg(res,'Authentication Failed',err.message)
+            })
+        }
+
+        if(password) {
+            User.findOne({password:password})
+            .then((authuser) => {
+                if(!authuser) {
+                    return Response.sendFailedmsg(res,'Authentcation Failed')
+                }
+                else {
+                    return Response.sendSuccessmsg(res,'Authenticated!',{isAdmin:authuser.isadmin,email:authuser.username})
+                }
+            })
+            .catch(err => {
+                return Response.sendFailedmsg(res,'Authentication Failed',err.message)
+            })
+        }
+
+     }
+     catch(err) {
+        return Response.sendFailedmsg(res,'Authentication Failed',err.message)
+     }
+ }
