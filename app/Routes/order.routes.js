e7711fdee9e4331efa185dcbf66f521240d9e38e
codewidhypno <bibinbabuman@gmail.com>
2021-03-01 19:48:08 +0530

    order details route updated

--- app/Routes/order.routes.js
+++ app/Routes/order.routes.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
     const order = require('../Controler/order.controller.js')
 
+    app.get('/order/orderDetails',order.getOrderDetails)
     app.post('/order',order.createOrder);
     app.get('/order',order.getOrders);
     app.get('/order/:id',order.getSingleOrder);
