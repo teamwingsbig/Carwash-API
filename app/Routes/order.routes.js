1420d395b70f2edf85aace201e88cc564d096d1e
codewidhypno <bibinbabuman@gmail.com>
2021-03-02 20:05:28 +0530

    added recent orders route

--- app/Routes/order.routes.js
+++ app/Routes/order.routes.js
@@ -2,6 +2,7 @@ module.exports = (app) => {
     const order = require('../Controler/order.controller.js')
 
     app.get('/order/orderDetails',order.getOrderDetails)
+    app.get('/order/recentOrders',order.getRecentOrders)
     app.post('/order',order.createOrder);
     app.get('/order',order.getOrders);
     app.get('/order/:id',order.getSingleOrder);
