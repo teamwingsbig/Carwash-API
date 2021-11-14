a0ad2688f9c0d68ac6fa906c1d4248e245e18060
Sufail Kalathil <sufailkalathil@gmail.com>
2021-11-14 22:02:46 +0530

    order image upload

--- app/Controler/customer.controller.js
+++ app/Controler/customer.controller.js
@@ -1,100 +1,99 @@
-const Customer  = require('../Models/customer.model')
+const Customer = require('../Models/customer.model')
 const Response = require('../helper/response')
 const path = require('path')
 
-exports.createCustomer=(req,res)=>{
-    try{
-        const { name, address, mobile,TRN} = req.body
-        if(!name  ) {
-            return Response.sendFailedmsg(res,'Please Fill All Fields')
-          }
+exports.createCustomer = (req, res) => {
+    try {
+        const {name, address, mobile, TRN, type} = req.body
+        if (!name) {
+            return Response.sendFailedmsg(res, 'Please Fill All Fields')
+        }
 
-          const customer = new Customer({
-            name :name,
-            address:address,          
-            mobile:mobile,     
-            trn:TRN     
-          })
-          
-             customer.save().then(company => {
-            return Response.sendSuccessmsg(res,'Customer Details Added')
+        const customer = new Customer({
+            name: name,
+            address: address,
+            mobile: mobile,
+            trn: TRN,
+            customerType: type
         })
-        .catch(err=>{
-          return Response.sendFailedmsg(res,'Failed To Add Customer Details',err.message)
+
+        customer.save().then(company => {
+            return Response.sendSuccessmsg(res, 'Customer Details Added')
         })
-          
-    }
-    catch{
+            .catch(err => {
+                return Response.sendFailedmsg(res, 'Failed To Add Customer Details', err.message)
+            })
+
+    } catch {
     }
 }
 
-exports.updateCustomer=(req,res)=>{
-    try{
-        const { name, address, mobile,TRN} = req.body
-        if(!name  ) {
-            return Response.sendFailedmsg(res,'Please Fill All Fields')
-          }
-          Customer.findOneAndUpdate({_id:req.params.id},{
-            name :name,
-            address:address,          
-            mobile:mobile,
-            trn:TRN            
-          }).then(company => {
-            return Response.sendSuccessmsg(res,'Customer Details Updated')
-          })
-          .catch(err => {
-            return Response.sendFailedmsg(res,'Failed To Update Customer Details',err.message)
-          })                                     
-    }
-    catch{
+exports.updateCustomer = (req, res) => {
+    try {
+        const {name, address, mobile, TRN, type} = req.body
+        if (!name) {
+            return Response.sendFailedmsg(res, 'Please Fill All Fields')
+        }
+        Customer.findOneAndUpdate({_id: req.params.id}, {
+            name: name,
+            address: address,
+            mobile: mobile,
+            trn: TRN,
+            customerType: type
+        }).then(company => {
+            return Response.sendSuccessmsg(res, 'Customer Details Updated')
+        })
+            .catch(err => {
+                return Response.sendFailedmsg(res, 'Failed To Update Customer Details', err.message)
+            })
+    } catch {
     }
 }
 exports.getCustomer = (req, res) => {
     try {
-     
-  
-        Customer.find({        
-          status: true
-      }).sort({createdAt: -1}).then((customer)=>{
+
+
+        Customer.find({
+            status: true
+        })
+            .populate('customerType', '_id name')
+            .sort({createdAt: -1}).then((customer) => {
             res.send(customer)
         })
-        .catch(err=>{
-            res.send([])
-        })
-       
-       
-    }
-    catch(e){
-     res.send([])
-    }
-  }
-  exports.getSingleCustomer =  (req, res) => {
+            .catch(err => {
+                res.send([])
+            })
 
-    try {
-  
-       Customer.findById(req.params.id).then((data)=>{
-           res.send({data})
-       })
-       .catch(err=>{
-           res.send([])
-       })      
-   
-    }
-    catch(error){
+
+    } catch (e) {
         res.send([])
     }
-  
-  }
-exports.deleteCustomer = (req, res) => {
-  try {
-    Customer.findOneAndUpdate({_id: req.params.id}, {status: false}).then((data) => {
-          return Response.sendSuccessmsg(res, 'Customer Deleted')
-      })
-          .catch(err => {
-              return Response.sendFailedmsg(res, 'Failed To Delete Customer', err.message)
-          })
-  } catch (err) {
-      return Response.sendFailedmsg(res, 'Failed To Delete Customer', err.message)
-  }
 }
-  
+exports.getSingleCustomer = (req, res) => {
+
+    try {
+
+        Customer.findById(req.params.id).then((data) => {
+            res.send({data})
+        })
+            .catch(err => {
+                res.send([])
+            })
+
+    } catch (error) {
+        res.send([])
+    }
+
+}
+exports.deleteCustomer = (req, res) => {
+    try {
+        Customer.findOneAndUpdate({_id: req.params.id}, {status: false}).then((data) => {
+            return Response.sendSuccessmsg(res, 'Customer Deleted')
+        })
+            .catch(err => {
+                return Response.sendFailedmsg(res, 'Failed To Delete Customer', err.message)
+            })
+    } catch (err) {
+        return Response.sendFailedmsg(res, 'Failed To Delete Customer', err.message)
+    }
+}
--- app/Models/customer.model.js
+++ app/Models/customer.model.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 
-const customerSchema  = mongoose.Schema({
-    name:{ type:String, required:true},
-    mobile:{ type:String, required:false},
-    address:{type:String, required:false},
-    trn:{type:String, required:false},
+const customerSchema = mongoose.Schema({
+    name: {type: String, required: true},
+    mobile: {type: String, required: false},
+    address: {type: String, required: false},
+    trn: {type: String, required: false},
     status: {type: Boolean, default: true},
-   
+    customerType: {type: mongoose.Types.ObjectId, ref: "customerTypes"},
+
 });
 
-module.exports = mongoose.model('customer_master',customerSchema); 
+module.exports = mongoose.model('customer_master', customerSchema);
