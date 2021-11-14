bdeb540a57d4b95517a246535daac4fc8177ebed
Sufail Kalathil <sufailkalathil@gmail.com>
2021-11-14 19:15:08 +0530

    edit delete added

--- app/Routes/customer-types.routes.js
+++ app/Routes/customer-types.routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 const customerType = require('../Controler/customerTypes.controller');
-const brand = require("../Controler/brand.controller");
 
 
 module.exports = () => {
